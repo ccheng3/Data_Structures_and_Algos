30d8098b13546be3364b47e2bd5d5ed45fd7a0bd
ccheng3 <ccrcheng@ucdavis.edu>
2020-08-30 13:16:33 -0700

    circular array resize in push()/pop() methods needs rework.

--- Queue/Circular_Array_based_Queue.cpp
+++ Queue/Circular_Array_based_Queue.cpp
@@ -11,6 +11,18 @@
  * equal to the fixed size. 
  * 
  * Otherwise, all of the operations work in O(1) time like with linked-list implementation.
+ * 
+ * 
+ * EDIT (08/29/2020):
+ * I am adding array resize() functionality to double the size of the array and copy the contents of the 
+ * full array (starting at front element and going to back element even in circular situation) plus the 
+ * new element into the new array, under the condition that you are pushing an element into a fully-occupied queue.
+ * 
+ * The queue therefore dynamically changes size to store an increased or decreased number of elements. 
+ * 
+ * Analogous functionality to halve the size of the array and copy the elements over, under the condition that 
+ * the number of elements in the array after the pop() is half the array's capacity.
+ * 
  */
 
 #include <iostream>
@@ -57,26 +69,72 @@ class Queue {
         bool isFull() const { return size == num_elements; }
         void Push(int data) {
             if (isFull()) {
-                cout << "Queue is full.\n";
-            }
-            else {
-                if (isEmpty()) {
-                    front = back = 0;
+                //cout << "Queue is full.\n";
+                // create a new temp array that has double the fixed size as current array
+                num_elements *= 2;
+                int* temp_array = new int[num_elements];
+                // copy the contents of current array into new array
+                if (front <= back) {
+                    for (int i = 0; i < num_elements; ++i) {
+                        *(temp_array + i) = *(my_array + i); 
+                    }
                 }
                 else {
-                    back = (back + 1) % num_elements;
+                    int j = 0;
+                    for (int i = front; i < size; ++i,++j) {
+                        *(temp_array + j) = *(my_array + i); 
+                    }
+                    for (int i = 0; i <= back; ++i,++j) {
+                        *(temp_array + j) = *(my_array + i); 
+                    }
                 }
-                *(my_array + back) = data;
-                ++size;
+                delete [] my_array;
+                // point the my_array pointer to the new temp array 
+                my_array = temp_array;
+                front = 0, back = size-1;
             }
+            if (isEmpty()) {
+                front = 0;
+            }
+            back = (back + 1) % num_elements;
+            *(my_array + back) = data;
+            ++size;
         }
         void Pop() {
             if (isEmpty()) {
                 cout << "Queue is empty.\n";
+                front = back = -1;
             }
             else {
                 front = (front + 1) % num_elements;
                 --size;
+
+                if (size == num_elements / 2) {
+                    // create new temp array that is half the fixed size as current array
+                    num_elements /= 2;
+                    int* temp_array = new int[num_elements];
+
+                    // copy elements of current array over to smaller array 
+                    int j = 0;
+                    if (front <= back) {
+                        for (int i = front; i <= back; ++i,++j) {
+                            *(temp_array + j) = *(my_array + i); 
+                        }
+                     }
+                    else {
+                        j = 0;
+                        for (int i = front; i < num_elements; ++i,++j) {
+                            *(temp_array + j) = *(my_array + i); 
+                        }
+                        for (int i = 0; i <= back; ++i,++j) {
+                            *(temp_array + j) = *(my_array + i); 
+                        }
+                    }
+                    delete [] my_array;
+                    my_array = temp_array;
+                    // reset front index to zero, and back index to (smaller array's size - 1)
+                    front = 0, back = num_elements - 1;
+                }
             }
         }
         // tried using the modulus technique to loop back and print circular array contents
@@ -104,33 +162,50 @@ class Queue {
         }
 };
 
+void Print_Details(Queue& my_queue) {
+    my_queue.Print_Data();
+    cout << "Front value is: " << my_queue.Get_Front() << endl;
+    cout << "Back value is: " << my_queue.Get_Back() << endl;
+    cout << "Queue contains: " << my_queue.Get_Size() << " elements.\n";
+}
+
 int main() {
     Queue my_queue;
 
     for (int i = 1; i <= 10; ++i) {
         my_queue.Push(i);
     }
-    my_queue.Print_Data();
-    // for (int i = 0; i < 2; ++i) {
+    Print_Details(my_queue);
+    for (int i = 0; i < 6; ++i) {
+        my_queue.Pop();
+    }
+    Print_Details(my_queue);
+    for (int i = 1; i <= 2; ++i) {
+        my_queue.Push(i);
+    }
+    Print_Details(my_queue);
+
+    for (int i = 0; i < 7; ++i) {
+        my_queue.Pop();
+    }
+    Print_Details(my_queue);
+
+    // for (int i = 0; i < 5; ++i) {
+    //     my_queue.Push((i+1) * 2000);
+    // }
+    // my_queue.Print_Data();
+    // cout << "Queue contains: " << my_queue.Get_Size() << " elements.\n";
+    // for (int i = 0; i < 3; ++i) {
+    //     my_queue.Push((i+1) * 43);
+    // }
+    // my_queue.Print_Data();
+    // cout << "Queue contains: " << my_queue.Get_Size() << " elements.\n";
+    // // my_queue.Print_Data();
+    // // my_queue.Push(4);
+    // for (int i = 0; i < 10; ++i) {
     //     my_queue.Pop();
     // }
     // my_queue.Print_Data();
-
-    for (int i = 0; i < 8; ++i) {
-        my_queue.Pop();
-    }
-    my_queue.Print_Data();
-
-    for (int i = 0; i < 5; ++i) {
-        my_queue.Push((i+1) * 2000);
-    }
-    my_queue.Print_Data();
-    cout << "Queue contains: " << my_queue.Get_Size() << " elements.\n";
-    for (int i = 0; i < 3; ++i) {
-        my_queue.Push((i+1) * 43);
-    }
-    cout << "Queue contains: " << my_queue.Get_Size() << " elements.\n";
-    my_queue.Print_Data();
-    my_queue.Push(4);
+    // //cout << "Queue contains: " << my_queue.Get_Size() << " elements.\n";
     return 0;
 }
